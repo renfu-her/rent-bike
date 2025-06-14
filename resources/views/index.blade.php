839998e6d8603c3d0a498046c0bcd9edd2070f83
Patrick Her <renfu.her@gmail.com>
2025-06-14 21:23:57 +0800

    調整首頁圖片顯示高度，將租借流程及特色圖片的高度從150px提升至200px，以增強視覺效果與使用者體驗。

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -32,19 +32,19 @@
 <div class="row text-center mb-5">
     <h3 class="mb-4">租借很簡單 4 步驟</h3>
     <div class="col-6 col-md-3 mb-4">
-        <img src="{{ asset('images/flow/flow-1.png') }}" class="mb-2" style="height:150px;">
+        <img src="{{ asset('images/flow/flow-1.png') }}" class="mb-2" style="height:200px;">
         <div>選擇旅遊地點</div>
     </div>
     <div class="col-6 col-md-3 mb-4">
-        <img src="{{ asset('images/flow/flow-2.png') }}" class="mb-2" style="height:150px;">
+        <img src="{{ asset('images/flow/flow-2.png') }}" class="mb-2" style="height:200px;">
         <div>選擇租借時間</div>
     </div>
     <div class="col-6 col-md-3 mb-4">
-        <img src="{{ asset('images/flow/flow-3.png') }}" class="mb-2" style="height:150px;">
+        <img src="{{ asset('images/flow/flow-3.png') }}" class="mb-2" style="height:200px;">
         <div>立即預約付訂</div>
     </div>
     <div class="col-6 col-md-3 mb-4">
-        <img src="{{ asset('images/flow/flow-4.png') }}" class="mb-2" style="height:150px;">
+        <img src="{{ asset('images/flow/flow-4.png') }}" class="mb-2" style="height:200px;">
         <div>前往門市取車</div>
     </div>
 </div>
@@ -53,19 +53,19 @@
 <div class="row text-center mb-5">
     <h3 class="mb-4">我們的特色</h3>
     <div class="col-6 col-md-3 mb-4">
-        <img src="{{ asset('images/feature/feature-1.png') }}" class="mb-2" style="height:150px;">
+        <img src="{{ asset('images/feature/feature-1.png') }}" class="mb-2" style="height:200px;">
         <div>簡單<br><span class="text-muted">輕鬆簡單的操作介面</span></div>
     </div>
     <div class="col-6 col-md-3 mb-4">
-        <img src="{{ asset('images/feature/feature-2.png') }}" class="mb-2" style="height:150px;">
+        <img src="{{ asset('images/feature/feature-2.png') }}" class="mb-2" style="height:200px;">
         <div>快速<br><span class="text-muted">快速流暢的租車體驗</span></div>
     </div>
     <div class="col-6 col-md-3 mb-4">
-        <img src="{{ asset('images/feature/feature-3.png') }}" class="mb-2" style="height:150px;">
+        <img src="{{ asset('images/feature/feature-3.png') }}" class="mb-2" style="height:200px;">
         <div>透明<br><span class="text-muted">透明公開的租車資訊</span></div>
     </div>
     <div class="col-6 col-md-3 mb-4">
-        <img src="{{ asset('images/feature/feature-4.png') }}" class="mb-2" style="height:150px;">
+        <img src="{{ asset('images/feature/feature-4.png') }}" class="mb-2" style="height:200px;">
         <div>安全<br><span class="text-muted">安全保障的旅遊體驗</span></div>
     </div>
 </div>
