0e44389628b3a5d11d086ba2e4ddc5f02611b88c
Patrick Her <renfu.her@gmail.com>
2025-06-21 12:06:06 +0800

    移除MemberResource中的批量刪除操作，簡化操作選項以提升使用者體驗。

--- app/Filament/Resources/MemberResource.php
+++ app/Filament/Resources/MemberResource.php
@@ -76,9 +76,9 @@ class MemberResource extends Resource
                 Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+                // Tables\Actions\BulkActionGroup::make([
+                //     Tables\Actions\DeleteBulkAction::make(),
+                // ]),
             ]);
     }
 
